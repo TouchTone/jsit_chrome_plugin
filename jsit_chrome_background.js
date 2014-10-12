461614b6cccee0c12cc4087ec99c52f9bec71875
MrPointVirgule <paul-louis@outlook.fr>
2014-10-12 14:46:41 +0200

    transformed Background page to Event page
    transformed Background page to Event page for better performances. Used
    those advices for context menu
    http://stackoverflow.com/questions/26245888/adding-context-menu-item-on-a-non-persistent-background-script
    . jsit_upload now works.

--- jsit_chrome_background.js
+++ jsit_chrome_background.js
@@ -175,11 +175,6 @@ function upload(info, tab)
 	chrome.tabs.sendMessage(tab.id, {type: "sendURL", url: url}); 
 }
 
-function readd(info, tab)
-{
-    chrome.tabs.sendMessage(tab.id, {type: "readdButtons"});
-}
-
 
 // Pattern learning
 
@@ -322,29 +317,38 @@ function learn(info, tab)
 }
 
 
-// Initialization
+	chrome.contextMenus.removeAll();
+	cm_upload = chrome.contextMenus.create({
+	        "id": "jsit_upload",
+	        "title": "Upload to JSIT", 
+	        "contexts": ["link"]
+	});
+	cm_learn = chrome.contextMenus.create({
+			"id": "jsit_learn",
+			"title": "Learn pattern", 
+			"contexts": ["link"]
+	});
+	cm_readd = chrome.contextMenus.create({
+			"id": "jsit_readd",
+			"title": "Re-add buttons", 
+			"contexts": ["page"]
+	});
 
-chrome.contextMenus.removeAll();
-// Almost never works... :( Need to look at again later.
-//cm_upload = chrome.contextMenus.create({
-//        "id": "jsit",
-//        "title": "Upload to JSIT", 
-//        "contexts": ["link"],
-//        "onclick": upload
-//});
-cm_learn = chrome.contextMenus.create({
-        "id": "jsit_learn",
-        "title": "Learn pattern", 
-        "contexts": ["link"],
-        "onclick": learn
-});
-cm_readd = chrome.contextMenus.create({
-        "id": "jsit_readd",
-        "title": "Re-add buttons", 
-        "contexts": ["page"],
-        "onclick": readd
-});
-
+	chrome.contextMenus.onClicked.addListener(function(info, tab) {
+		switch(info.menuItemId) {
+			case "jsit_readd":
+				chrome.tabs.sendMessage(tab.id, {type: "readdButtons"});
+				break;
+			case "jsit_learn":
+				learn(info, tab);
+				break;
+			case "jsit_upload":
+				upload(info, tab);
+				break;
+		}
+	});
+	
+	
 // Message listener
 
 chrome.runtime.onMessage.addListener(
